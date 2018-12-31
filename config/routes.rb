eca4b56daddc084174479680d4b351dc4940a615
Jan <jan.spoerer@whu.edu>
2018-12-31 16:53:21 +0100

    Fixed existing bugs
    Brought project into a clean state, tickets can now be worked on step-by-step. Suggested next ticket: features/cancel-ticket to cancel/close a ticket.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  scope '(:locale)', locale: /de/ do
+  scope '(:locale)', locale: /#{I18n.available_locales.join("|")}/ do
     root to: 'pages#home'
       resources :tickets, only: [:new, :create, :show, :index] do
         resources :comments
+        get 'close', to: "tickets#close"
       end
   end
 end
